53179d0b18f000120c0a001817bf521652d47c22
Esteban Luchsinger <eluchsingerm@hotmail.com>
2018-07-14 14:27:53 +0200

    Added more tests for the efcore project

--- SlimRepository.EntityFrameworkCore.Test/RepositoryTests.cs
+++ SlimRepository.EntityFrameworkCore.Test/RepositoryTests.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using SlimRepository.EntityFrameworkCore.Test.Data;
@@ -7,48 +8,143 @@ namespace SlimRepository.EntityFrameworkCore.Test
 {
     public partial class RepositoryTests
     {
-        [Fact]
-        public void GivenDatabaseIsEmpty_ItShouldAddAnObject()
+        public class Add
         {
-            var options = new DbContextOptionsBuilder<TestContext>()
-                .UseInMemoryDatabase(Helper.GetCallerName())
-                .Options;
-            TestObject addedObject;
-
-            using (var context = new TestContext(options))
+            [Fact]
+            [Trait("Category", "Add")]
+            public void GivenDatabaseIsEmpty_ItShouldAddAnObject()
             {
-                var repository = new Repository<TestObject>(context);
-                addedObject = repository.Add(new TestObject { Name = "Test" });
-            }
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+                TestObject addedObject;
 
-            using (var context = new TestContext(options))
-            {
-                var returnedObject = context.TestObjects.Find(addedObject.Id);
-                addedObject.Should().NotBeNull("an object should be added");
-                returnedObject.Should().BeEquivalentTo(addedObject);
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+                    addedObject = repository.Add(new TestObject {Name = "Test"});
+                }
+
+                using (var context = new TestContext(options))
+                {
+                    var returnedObject = context.TestObjects.Find(addedObject.Id);
+                    addedObject.Should().NotBeNull("an object should be added");
+                    returnedObject.Should().BeEquivalentTo(addedObject);
+                }
             }
         }
 
-        [Fact]
-        public void GivenDatabaseHasObjects_ItShouldRemoveFirstObject()
+        public class Delete
         {
-            var options = new DbContextOptionsBuilder<TestContext>()
-                .UseInMemoryDatabase(Helper.GetCallerName())
-                .Options;
-            var seedData = options.EnsureSeeded();
 
-            using (var context = new TestContext(options))
+            [Fact]
+            [Trait("Category", "Delete")]
+            public void GivenRemovingEntityNotInStore_ItShouldThrow()
             {
-                var repository = new Repository<TestObject>(context);
-                repository.Delete(seedData[0]);
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+                    repository.Invoking(r => r.Delete(new TestObject()))
+                        .Should().Throw<DbUpdateConcurrencyException>()
+                        .WithMessage("Attempted to update or delete an entity that does not exist in the store.");
+                }
             }
 
-            using (var context = new TestContext(options))
+            [Fact]
+            [Trait("Category", "Delete")]
+            public void GivenDatabaseHasObjects_ItShouldRemoveFirstObject()
             {
-                var foundObject = context.TestObjects.Find(seedData[0].Id);
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+                var seedData = options.EnsureSeeded();
+
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+                    repository.Delete(seedData[0]);
+                }
+
+                using (var context = new TestContext(options))
+                {
+                    var foundObject = context.TestObjects.Find(seedData[0].Id);
+                    foundObject.Should().BeNull();
+                }
+            }
+        }
+
+        public class GetById
+        {
+
+            [Fact]
+            [Trait("Category", "GetById")]
+            public void GivenDatabaseHasObjects_ItShouldReturnOneById()
+            {
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+                var seedData = options.EnsureSeeded();
+                var expectedObject = seedData[0];
+                TestObject foundObject;
+
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+                    foundObject = repository.GetById(expectedObject.Id);
+                }
+
+                foundObject.Should().BeEquivalentTo(expectedObject);
+            }
+
+            [Fact]
+            [Trait("Category", "GetById")]
+            public void GivenDatabaseHasNoObjects_ItShouldReturnNullById()
+            {
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+                TestObject foundObject;
+
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+                    foundObject = repository.GetById(0);
+                }
+
                 foundObject.Should().BeNull();
             }
         }
+        
+        public class Edit
+        {
+            [Fact]
+            [Trait("Category", "Edit")]
+            public void GivenObjectContainedInDatabaseIsEdited_ItShouldBePersisted()
+            {
+                var options = new DbContextOptionsBuilder<TestContext>()
+                    .UseInMemoryDatabase(Helper.GetCallerName())
+                    .Options;
+                var seedData = options.EnsureSeeded();
+                var changedObject = seedData[0];
 
+                using (var context = new TestContext(options))
+                {
+                    var repository = new Repository<TestObject>(context);
+
+                    changedObject.Name = "EditedObject";
+                    repository.Edit(changedObject);
+                }
+
+                using (var context = new TestContext(options))
+                {
+                    var foundObject = context.Find<TestObject>(changedObject.Id);
+                    foundObject.Should().BeEquivalentTo(changedObject);
+                }
+            }
+        }
     }
 }
